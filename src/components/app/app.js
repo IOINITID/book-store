620dabe2a14aa1aa32e92f0e0e621f2b645af956
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-11-21 21:10:06 +0300

    Updated path for pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,22 +6,27 @@ import CartPage from '../pages/cart-page';
 import ShopHeader from '../shop-header/shop-header';
 
 const App = () => {
-    return (
-        <main role="main" className="container">
-            <ShopHeader numItems={5} total={210}/>
-            <Switch>
-                <Route
-                    path="/"
-                    component={HomePage}
-                    exact
-                />
-                <Route
-                    path="/cart"
-                    component={CartPage}
-                />
-            </Switch>
-        </main>
-    );
+  return (
+    <main role="main" className="container">
+      <ShopHeader numItems={5} total={210} />
+      <Switch>
+        <Route
+          path="/book-store"
+          component={HomePage}
+          exact
+        />
+        <Route
+          path="/book-store/index.html"
+          component={HomePage}
+          exact
+        />
+        <Route
+          path="/cart"
+          component={CartPage}
+        />
+      </Switch>
+    </main>
+  );
 };
 
 export default App;
