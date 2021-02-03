e5138a8d8fca2c47004ab0495b8c79206c08b405
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-02-03 14:17:05 +0300

    Added book-rating component

--- src/components/book/book.tsx
+++ src/components/book/book.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import styles from './book.scss';
-import CartIcon from '../../assets/images/cart-icon.svg';
 import BookCover from '../book-cover/book-cover';
-import { getColorByGenre } from '../../utils/common';
+import BookInfo from '../book-info/book-info';
 
 interface IBook {
   book: {
@@ -26,34 +25,10 @@ const Book = (props: IBook) => {
     book: { title, author, publisher, release, pages, cover, age, image, rating, price, genres, description },
   } = props;
 
-  const genresItems = genres.map((item, index) => {
-    const background = getColorByGenre(item);
-
-    return (
-      <li className={styles['genre-item']} key={item + index}>
-        <span className={styles['genre']} style={{ backgroundColor: background }}>
-          {item}
-        </span>
-      </li>
-    );
-  });
-
-  const bookCover = { title, image, rating };
-
   return (
     <div className={styles['book']}>
-      <BookCover bookCover={bookCover} />
-      <div className={styles['info']}>
-        <h3 className={styles['title']}>{title}</h3>
-        <p className={styles['author']}>{author}</p>
-        <ul className={styles['genre-list']}>{genresItems}</ul>
-        <div className={styles['cart']}>
-          <span className={styles['price']}>{price.toLocaleString()} ₽</span>
-          <button className={styles['button-buy']} type="button" title="Добавить в корзину">
-            <CartIcon width="12" height="13" />
-          </button>
-        </div>
-      </div>
+      <BookCover bookCover={{ title, image, rating }} />
+      <BookInfo bookInfo={{ title, author, price, genres }} />
     </div>
   );
 };
