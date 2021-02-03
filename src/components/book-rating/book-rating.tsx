7b472b5320e5ca1c53abb04fe22a731b67212033
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-02-03 16:50:51 +0300

    Revert "Revert "Added book-rating component""
    This reverts commit 663800f0863eafb7a2545292db0e0d35eef1da03.

---
+++ src/components/book-rating/book-rating.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './book-rating.scss';
+import RatingIcon from '../../assets/images/rating-icon.svg';
+
+interface IBookRating {
+  rating: number;
+}
+
+const BookRating = (props: IBookRating) => {
+  const { rating } = props;
+
+  return (
+    <div className={styles['rating']}>
+      <RatingIcon className={styles['rating-icon']} width="8" height="8" />
+      <span className={styles['rating-item']}>{rating.toFixed(1)}</span>
+    </div>
+  );
+};
+
+export default BookRating;
