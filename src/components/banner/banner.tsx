907786c892ca2bd63c46791d67c679cf111f6994
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-02-04 00:13:24 +0300

    Added banner images

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import './banner.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Pagination } from 'swiper';
+import SwiperCore, { Pagination, Autoplay } from 'swiper';
 import 'swiper/swiper.scss';
-SwiperCore.use([Pagination]);
+SwiperCore.use([Pagination, Autoplay]);
 
 const Banner = () => {
   return (
     <div className="banner-container">
-      <Swiper pagination={{ clickable: true }} slidesPerView={1} spaceBetween={0}>
+      <Swiper
+        pagination={{ clickable: true }}
+        slidesPerView={1}
+        spaceBetween={0}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        loop={true}
+      >
         <SwiperSlide>
-          <div className="banner"></div>
+          <div className="banner">
+            <img src="images/atomic-design-banner.jpg" alt="" />
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="banner"></div>
+          <div className="banner">
+            <img src="images/twilight-banner.jpg" alt="" />
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="banner"></div>
+          <div className="banner">
+            <img src="images/1984-banner.jpg" alt="" />
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="banner"></div>
