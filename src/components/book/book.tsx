6decf0464bc588ffe19d5f6db54f04f217d904b1
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-02-08 00:46:14 +0300

    Updated book component

--- src/components/book/book.tsx
+++ src/components/book/book.tsx
@@ -3,18 +3,34 @@ import './book.scss';
 import BookCover from '../book-cover/book-cover';
 import BookInfo from '../book-info/book-info';
 import { IBook } from '../../interfaces';
+import { connect } from 'react-redux';
+import { showModalAction } from '../../actions';
 
-const Book = (props: { book: IBook }) => {
+const Book = (props: { book: IBook; showModal: (id) => void }) => {
   const {
     book: { id, title, author, publisher, release, pages, cover, age, image, rating, price, genres, description },
+    showModal,
   } = props;
+
+  const onBookClick = (evt) => {
+    evt.preventDefault();
+    showModal(id);
+  };
 
   return (
     <div className="book">
-      <BookCover title={title} image={image} rating={rating} />
-      <BookInfo bookInfo={{ title, author, price, genres }} />
+      <a className="book__link" href="#ref" onClick={onBookClick}>
+        <BookCover title={title} image={image} rating={rating} />
+        <BookInfo bookInfo={{ title, author, price, genres }} />
+      </a>
     </div>
   );
 };
 
-export default Book;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    showModal: (id) => dispatch(showModalAction(id)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Book);
