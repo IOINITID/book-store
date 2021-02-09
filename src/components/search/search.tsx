cb9e1cd124882b30f6337fd18620ccf95a48cc85
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-02-09 22:45:07 +0300

    Updated search component

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -8,6 +8,12 @@ const Search = () => {
       <label className="search__label" htmlFor="">
         <input className="search__input" type="search" placeholder="Искать книгу" />
         <SearchIcon className="search__icon" />
+        <ul className="search__list">
+          <li>
+            <p>Хроники Нарнии</p>
+            <p>Клайв Льюис</p>
+          </li>
+        </ul>
       </label>
     </form>
   );
