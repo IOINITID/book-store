70f139e2659235d34c0bfbe94c834f705ed1907e
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-02-10 19:09:25 +0300

    Updated cart component

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -14,21 +14,23 @@ const Cart = (props: { cartBooks: IBook[]; addToCart: (id) => void; removeFromCa
         {cartBooks.map((book, index) => {
           return (
             <li key={book.title + index}>
-              <span>{index + 1}</span>
-              <img src={`images/${book.image}-1.jpg`} width="136" height="160" alt="" />
-              <p>{book.title}</p>
-              <p>{book.author}</p>
-              <div className="quantity">
-                <button type="button" onClick={() => removeFromCart(book.id)}>
+              <span className="cart__id">{index + 1}</span>
+              <img className="cart__image" src={`images/${book.image}-1.jpg`} width="136" height="160" alt="" />
+              <p className="cart__title">{book.title}</p>
+              <p className="cart__author">{book.author}</p>
+              <div className="cart__quantity">
+                <button className="cart__button" type="button" onClick={() => removeFromCart(book.id)}>
                   -
                 </button>
                 <span>{book.quantity}</span>
-                <button type="button" onClick={() => addToCart(book.id)}>
+                <button className="cart__button" type="button" onClick={() => addToCart(book.id)}>
                   +
                 </button>
               </div>
-              <p>{book.price} ₽</p>
-              <button type="button">Удалить</button>
+              <p className="cart__price">{book.price} ₽</p>
+              <button className="cart__remove" type="button">
+                -
+              </button>
             </li>
           );
         })}
