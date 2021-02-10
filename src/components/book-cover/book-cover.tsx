8b2aebd01e96a0c2957a46560b00fef5c85f45f6
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-02-10 16:03:35 +0300

    Updated book-cover and book-rating components

--- src/components/book-cover/book-cover.tsx
+++ src/components/book-cover/book-cover.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-// import './book-cover.scss';
+import './book-cover.scss';
 import BookRating from '../book-rating/book-rating';
 import { IBookCover } from '../../interfaces';
-import styled from 'styled-components';
-
-const Cover = styled.div`
-  position: relative;
-  background-color: #d4d4d4;
-`;
-
-const CoverImage = styled.img`
-  object-fit: cover;
-`;
-
-const CoverRating = styled(BookRating)`
-  position: absolute;
-  top: 0;
-  right: 0;
-`;
 
 const BookCover = (props: IBookCover) => {
   const { title, image, rating } = props;
 
   return (
-    <Cover>
-      <CoverImage
+    <div className="cover">
+      <img
         className="cover__image"
         src={`images/${image}-1.jpg`}
         width="136"
@@ -32,8 +16,8 @@ const BookCover = (props: IBookCover) => {
         loading="lazy"
         alt={`Обложка книги ${title}.`}
       />
-      <CoverRating rating={rating} />
-    </Cover>
+      <BookRating rating={rating} />
+    </div>
   );
 };
 
--- src/components/book-rating/book-rating.tsx
+++ src/components/book-rating/book-rating.tsx
@@ -1,38 +1,16 @@
 import React from 'react';
-// import './book-rating.scss';
+import './book-rating.scss';
 import RatingIcon from '../../assets/images/rating-icon.svg';
 import { IBookRating } from '../../interfaces';
-import styled from 'styled-components';
-
-const Rating = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: auto auto;
-  gap: 4px;
-  padding: 4px 11px;
-  background: #f5f5f5;
-  border-radius: 0 0 0 8px;
-`;
-
-const RatingItem = styled.div`
-  font-weight: 300;
-  font-size: 12px;
-  line-height: 14px;
-  color: #5e5e5e;
-`;
 
 const BookRating = (props: IBookRating) => {
-  const { className, rating } = props;
+  const { rating } = props;
 
   return (
-    // <div className="rating">
-    //   <RatingIcon className="rating-icon" width="8" height="8" />
-    //   <span className="rating-item">{rating.toFixed(1)}</span>
-    // </div>
-    <Rating className={className}>
-      <RatingIcon width="8" height="8" />
-      <RatingItem>{rating.toFixed(1)}</RatingItem>
-    </Rating>
+    <div className="rating">
+      <RatingIcon className="rating-icon" width="8" height="8" />
+      <span className="rating-item">{rating.toFixed(1)}</span>
+    </div>
   );
 };
 
